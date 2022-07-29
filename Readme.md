877d99bd9c7ebfc99494d66e7370c32734b18f9b
Andre-N <aandre.nidec@gmail.com>
2022-07-29 08:15:20 -0300

    Segundo Update - 29/07/2022

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Projeto Intranet Nidec
+
+Esse projeto se propõe a automatizar e digitalizar processos interno da Empresa, livrando da dependencia de planilha e outros softwares que ocupam espaço.
---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início</title>
+    <script src="script.js"></script>
+    <style><?php include ("C:/xampp/htdocs/nidec/style.css"); ?></style>
+</head>
+<body>
+<header class="topo">
+    <img src="https://integrare.embraco.com.br/Embraco/images/nidec.png" alt="Nidec GA"></img>
+    <ul>
+            <li>Inicio</li>
+            <li>Departamentos</li>
+            <li></li>
+    </ul>
+
+    <a href="">Login</a>
+</header>
+
+
+</body>
+</html>
