b80cb33548a5597e5c621db2494d326cc8c524f6
Andre-N <aandre.nidec@gmail.com>
2022-08-09 17:30:46 -0300

    Quarto Update - 09/08/2022

---
+++ src/CRUD/crud.php
@@ -0,0 +1,33 @@
+<style><?php include (__DIR__.'/./crudStyle.css')?></style>
+
+
+<table class="Tabela">
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Nome</td>
+            <td>Sobrenome</td>
+            <td>Data de Nascimento</td>
+            <td>Gênero</td>
+            <td>Foto</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>André</td>
+            <td>Silva</td>
+            <td>15/08/2001</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>01</td>
+            <td>André</td>
+            <td>Silva</td>
+            <td>15/08/2001</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
