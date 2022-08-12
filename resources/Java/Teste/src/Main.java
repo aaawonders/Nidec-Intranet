b39b7b25512c40448831b6ef0c8642d65fb40855
Andre-N <aandre.nidec@gmail.com>
2022-08-11 21:14:17 -0300

    Quarto Update - 11/08/2022

--- resources/Java/Teste/src/Main.java
+++ resources/Java/Teste/src/Main.java
@@ -1,6 +1,31 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        JFrame f = new JFrame();
+        JPanel Panel = new JPanel();
+
+
+        f.setTitle("Janela");
+        f.setSize(300,200);
+        f.setLocation(500,300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel Label = new JLabel("Teste");
+        JTextField txField = new JTextField(10);
+        JButton Button = new JButton("OK");
+
+        Panel.add(Label);
+        Panel.add(txField);
+        Panel.add(Button);
+
+        f.add(Panel);
+        f.setVisible(true);
+
+
+        String Texto = txField.toString();
+
+        System.out.println(Texto);
+
     }
 }
