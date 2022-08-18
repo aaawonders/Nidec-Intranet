168aa30b67c5f8bd7c7529ffa0985a6af747c85a
aaawonders <realandre15@gmail.com>
2022-08-18 06:33:31 -0300

    Quinto Update - 18/08/2022

---
+++ src/Mailer/get_auth_token.php
@@ -0,0 +1,5 @@
+<?php
+
+$redirectUri = 'http://localhost/nidec/src/mailer/get_oauth_token.php/';
+$clientId = '542356575387-f3cqihfbb2a3tg9qdago600k45u9j1bt.apps.googleusercontent.com';
+$clientSecret = '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';
